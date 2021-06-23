1161cc1fa40992628ffdb36073a9aa40e54fe4e8
Aishwarya Kashyup <ash0492@gmail.com>
2021-06-23 09:08:13 +0530

    changes made to ElectionWinner

--- ElectionWinner.py
+++ ElectionWinner.py
@@ -1,10 +1,26 @@
 from collections import Counter
 def ElectionWinner(arr,n):
-    for i in range(0,n):
-        print(arr,arr.count(arr[i]))
+    temp=Counter(arr)
+    dict={}
+    for value in temp.values():
+        dict[value]=[]
 
 
-arr=["john","johnny","jackie","johnny","john" "jackie","jamie","jamie","john","johnny","jamie","johnny","john"]
+    for (key,value) in temp.items():
+        dict[value].append(key)
+
+
+    maxVote=sorted(dict.keys(),reverse=True)[0]
+
+
+    if(len(dict[maxVote])>1):
+        print(sorted(dict[maxVote])[0],maxVote)
+    else:
+        print(dict[maxVote][0], maxVote)
+
+
+
+arr=["john","johnny","jackie","johnny","john","jackie","jamie","jamie","john","johnny","jamie","johnny","john"]
 #arr=["andy","blake","clark"]
-n =3
+n =13
 ElectionWinner(arr,n)
